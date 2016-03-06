32eee08ac6bf4a2cd60a54adf8dc402f324f4b2f
Mateusz Szynka <devmsw12@gmail.com>
2016-03-06 15:31:00 +0100

    Tokenizer, basic auth and DB finalize

--- router.js
+++ router.js
@@ -1,10 +1,30 @@
 var db = require('./db.js');
+var token = require('./token.js');
+
+function authenticate(req, res, next) {
+	try {
+		if (req.headers.apptokenizer === token) {
+			var user = 'webapp';
+			next();
+		} else {
+			res.status(401).json({
+				message: "Incorrect token credentials"
+			});
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(401).json({
+			message: "Incorrect token credentials"
+		});
+	}
+}
+
 
 module.exports.route = (app) => {
-	app.get('/', db.orm.getTables);
-	app.get('/:table', db.orm.all);
-	app.get('/:table/:id', db.orm.findById);
-	app.post('/:table', db.orm.add);
-	app.put('/:table/:id', db.orm.update);
-	app.delete('/:table/:id', db.orm.delete);
+	app.get('/', authenticate, db.orm.getTables);
+	app.get('/:table', authenticate, db.orm.all);
+	app.get('/:table/:id', authenticate, db.orm.findById);
+	app.post('/:table', authenticate, db.orm.add);
+	app.put('/:table/:id', authenticate, db.orm.update);
+	app.delete('/:table/:id', authenticate, db.orm.delete);
 }
